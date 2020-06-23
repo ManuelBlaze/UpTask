41a496b7603dc7ed2d3f5074b937a92a2c334dcb
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-22 19:37:27 -0500

    Conexion

--- includes/funciones/funciones.php
+++ includes/funciones/funciones.php
@@ -1,7 +1,9 @@
-<?php
+<?php 
 
     function obtenerPaginaActual() {
         $archivo = basename($_SERVER['PHP_SELF']);
         $pagina = str_replace(".php", "", $archivo);
         return $pagina;
     }
+
+?>
