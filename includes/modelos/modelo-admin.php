83d36c7c41f99610781e4acc46cac56365fb8084
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-22 22:59:09 -0500

    Hashear Password

--- includes/modelos/modelo-admin.php
+++ includes/modelos/modelo-admin.php
@@ -1,4 +1,20 @@
 <?php
 
-    die(json_encode($_POST));
+    $accion = $_POST['accion'];
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password'];
+    
+    if ($accion === 'crear') {
+        //Hashear passwords
+        $opciones = array(
+            'cost' => 10
+        );
+        $hash_password = password_hash($password, PASSWORD_BCRYPT, $opciones);
+
+        $respuesta = array (
+            'pass' => $hash_password
+        );
+        echo json_encode($respuesta);
+    }
+
 ?>
