335dfa75c9cc348f4777e86743c11184ad68844c
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-30 16:03:14 -0500

    Favicon

--- includes/funciones/conexion.php
+++ includes/funciones/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = new mysqli('localhost','root', 'root', 'uptask');
+    $conn = new mysqli('us-cdbr-east-02.cleardb.com','b0829dbf1609ed', '36d846d2', 'heroku_9c32420ff1b6f10');
 
     if ($conn->connect_error) {
         echo $conn->connect_error;
